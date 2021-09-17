438816c6a299da39e5d8fa2e257e65bbe5fc6b78
stevenjhomem <sjhomem@outlook.com>
2021-09-17 12:07:10 -0500

    finished some tests

--- src/components/Color.test.js
+++ src/components/Color.test.js
@@ -5,15 +5,43 @@ import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Color from './Color';
 
+const color = { color: "pink", code: { hex: "#FFC0CB" }, id:100};
+
 test("Renders without errors with blank color passed into component", () => {
+    const color = { color: "", code: { hex: "" }, id:100};
+
+    render(<Color color={color}/>);
 });
   
-test("Renders the color passed into component", () => {
+test("Renders the color passed into the component", () => {
+    render(<Color color={color}/>);
+
+    const colorItem = screen.queryByText('pink');
+    expect(colorItem).toBeInTheDocument();
 });
 
-test("Executes handleDelete and toggleEdit property when the 'x' icon is clicked", () => {
+test("Executes handleDelete and toggleEdit when the 'x' icon is clicked", () => {
+    const deleteMock = jest.fn();
+    const toggleMock = jest.fn();
+
+    render(<Color color={color} deleteColor={deleteMock} toggleEdit={toggleMock}/>);
+
+    const deleteItem = screen.queryByTestId('delete');
+    userEvent.click(deleteItem);
+
+    expect(deleteMock).toBeCalled();
+    expect(toggleMock).toBeCalled();
 });
 
-test("Executes setEditColor and toggleEdit property when color div is clicked", () => {
-    
+test("Executes setEditColor and toggleEdit when color div is clicked", () => {
+    const editMock = jest.fn();
+    const toggleMock = jest.fn();
+
+    render(<Color color={color} setEditColor={editMock} toggleEdit={toggleMock}/>);
+
+    const colorItem = screen.queryByTestId('color');
+    userEvent.click(colorItem);
+
+    expect(editMock).toBeCalled();
+    expect(toggleMock).toBeCalled();
 });
--- src/components/ColorList.test.js
+++ src/components/ColorList.test.js
@@ -5,10 +5,39 @@ import { render, screen} from "@testing-library/react";
 import ColorList from './ColorList';
 
 test("Renders an empty list of colors without errors", () => {
+    const colors=[];
+
+    render(<ColorList colors={colors}></ColorList>)
 });
 
 test("Renders a list of colors without errors", () => {
+    const colors = [
+        {
+            color: 'pink',
+            code: {
+                hex: "#FFC0CB", 
+            },
+            id: 100
+        },
+        {
+            color: 'blue',
+            code: {
+                hex: "#0000FF", 
+            },
+            id: 101
+        },
+    ];
+
+    render(<ColorList colors={colors}></ColorList>)
 });
 
 test("Renders the EditForm when editing = true and does not render EditForm when editing = false", () => {
+    const { rerender } = render(<ColorList colors={[]} editing={true}/>);
+    let editMenu = screen.queryByTestId('edit_menu');
+    expect(editMenu).toBeInTheDocument();
+
+    rerender(<ColorList colors={[]} editing={false}/>);
+    editMenu = screen.queryByTestId('edit_menu');
+
+    expect(editMenu).not.toBeInTheDocument();
 });
